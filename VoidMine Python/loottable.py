9bacbe0d1ceb7ec824f1169a6d28d68d9f0dce68
SmolCube0 <smolcube0@gmail.com>
2025-09-12 23:25:56 -0700

    made prototype loottable

--- VoidMine Python/loottable.py
+++ VoidMine Python/loottable.py
@@ -3,3 +3,25 @@ import json
 from pathlib import Path
 
 resourceStats = Path("resourcestats.json")
+import random as rand
+# fix json stuff, merge lootTable with resourceStats
+
+lootTable = {
+    "wood":100,
+    "coal":80,
+    "gold":25,
+    "ruby":5,
+    "diamond":1
+}
+
+lootItems = list(lootTable.keys())
+lootChances = list(lootTable.values())
+"""drops = rand.choices(lootItems,weights=lootChances,k=len(lootItems))
+print(drops)"""
+drops = []
+for i in range(len(lootItems)):
+    roll = rand.uniform(0,100)
+    if roll <= lootChances[i]:
+        drops.append(lootItems[i])
+
+print(drops)
