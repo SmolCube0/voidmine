8839cad0e65a5f95286724c6236a33bdfabe188e
SmolCube0 <smolcube0@gmail.com>
2025-09-12 22:56:08 -0700

    started work on python loot table

---
+++ playground.py
@@ -0,0 +1,12 @@
+import random as rand
+
+lootTable = {
+    "wood":100,
+    "coal":80,
+    "gold":25,
+    "ruby":5,
+    "diamond":1
+}
+
+print(lootTable[1])
+# loot = [rand.randint(1,100) if lootTable[1]]
